207c0feb12568aa2d3a1801a2a51e548e7cb4e94
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-23 17:38:53 +0530

    chore: Move color details below the color box

--- components/ColorPalette.tsx
+++ components/ColorPalette.tsx
@@ -2,29 +2,31 @@ import React from "react";
 
 const ColorPalette = () => {
   const colors = [
+    // Light
     { name: "Light Purple", hex: "#B683FF", description: "Gradient color" },
-    { name: "Dark Purple", hex: "#8000FF", description: "Gradient color" },
     { name: "Light Input", hex: "#858585", description: "Dark Gray" },
-    { name: "Dark Input", hex: "#858585", description: "Midnight Gray" },
-    { name: "Dark Background", hex: "#121E2E", description: "Very Dark Blue" },
     {
       name: "Light Background",
       hex: "#FAFAFA",
       description: "Very Light Gray",
     },
     { name: "Light Elements", hex: "#FFFFFF", description: "White" },
-    { name: "Dark Elements", hex: "#283640", description: "Dark Blue" },
     { name: "Light Text", hex: "#FFFFFF", description: "White" },
+    // Dark
     { name: "Dark Text", hex: "#0D1C28", description: "Very Dark Blue" },
+    { name: "Dark Purple", hex: "#8000FF", description: "Gradient color" },
+    { name: "Dark Input", hex: "#858585", description: "Midnight Gray" },
+    { name: "Dark Background", hex: "#121E2E", description: "Very Dark Blue" },
+    { name: "Dark Elements", hex: "#283640", description: "Dark Blue" },
   ];
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-4 lg:grid-cols-5">
       {colors.map((color) => (
-        <div
-          key={color.name}
-          className="border-2 border-black p-4 text-center"
-          style={{ backgroundColor: color.hex }}>
+        <div key={color.name} className="text-lg font-semibold">
+          <div
+            className="border-2 border-black p-4 text-center"
+            style={{ backgroundColor: color.hex }}></div>
           <div>{color.name}</div>
           <div>{color.hex}</div>
           <div>{color.description}</div>
