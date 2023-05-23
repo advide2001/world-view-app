013329a7861fcc484b62731dbc85277f675c9980
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-23 16:54:16 +0530

    feat: Extend Tailwind CSS config with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // Gradient colors
+        "light-purple": "#B683FF",
+        "dark-purple": "#8000FF",
+
+        // Inputs
+        // dark-gray
+        "light-input": "#858585",
+        // midnight-gray
+        "dark-input": "#858585",
+
+        // Backgrounds
+        // very-dark-blue
+        "dark-bg": "#121E2E",
+        // very-light-gray
+        "light-bg": "#FAFAFA",
+
+        // Elements
+        // white
+        "light-elements": "#FFFFFF",
+        // dark-blue
+        "dark-elements": "#283640",
+
+        // Text
+        // white
+        "dark-text": "#FFFFFF",
+        // very-dark-blue
+        "light-text": "#0D1C28",
       },
     },
   },
   plugins: [],
-}
+};
