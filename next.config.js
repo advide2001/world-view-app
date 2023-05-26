6dac610f32226738eb96e6b9683e35f90e9a22fc
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-26 22:56:51 +0530

    feat: Add remote image patterns for flag images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        port: "",
+        pathname: "/w320/**",
+      },
+    ],
+  },
+};
