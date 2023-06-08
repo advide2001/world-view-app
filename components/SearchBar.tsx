1f3b5c04d73e18f9203ca626f809e9af7506a0eb
Adarsh Desai <desaiadarsh22@gmail.com>
2023-06-08 16:48:07 +0530

    refactor: Enhance SearchBar component with props and improved functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,31 +1,41 @@
 "use client";
 
-import React from "react";
+import React, { FC } from "react";
 import TextInput from "./TextInput";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
-interface Props {}
+interface Props {
+  searchStringValue: string;
+  setSearchStringValue: React.Dispatch<React.SetStateAction<string>>;
+}
 
-const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  event.preventDefault();
-};
-
-const SearchBar = () => (
-  <form onSubmit={handleSubmit}>
-    <div className="flex items-center gap-4 rounded-md bg-light-elements px-2 py-4 text-light-input">
-      <MagnifyingGlassIcon className="h-6 w-6" />
-      <label htmlFor="ipAddress" className="sr-only">
-        Search for a country...
-      </label>
-      <TextInput
-        id="search-bar"
-        placeholder="Search for a country..."
-        value=""
-        onChange={() => {}}
-        className="bg-light-elements focus:outline-none"
-      />
-    </div>
-  </form>
-);
+function SearchBar({
+  searchStringValue,
+  setSearchStringValue,
+}: Props): React.ReactElement {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setSearchStringValue("");
+  };
+  return (
+    <form className="w-full" onSubmit={handleSubmit}>
+      <div className="flex items-center gap-4 rounded-md  bg-light-elements px-2 py-4 text-light-input">
+        <MagnifyingGlassIcon className="h-6 w-6" />
+        <label htmlFor="ipAddress" className="sr-only">
+          Search for a country...
+        </label>
+        <TextInput
+          id="search-bar"
+          placeholder="Search for a country..."
+          value={searchStringValue}
+          onChange={(e) => {
+            setSearchStringValue(e.target.value);
+          }}
+          className="flex-1 bg-light-elements font-semibold focus:outline-none"
+        />
+      </div>
+    </form>
+  );
+}
 
 export default SearchBar;
