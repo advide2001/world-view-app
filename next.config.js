5f209ffb5669ba4b44151199760790092459b878
Adarsh Desai <desaiadarsh22@gmail.com>
2023-06-09 22:34:00 +0530

    feat: Add configuration to exclude "/app/design-system/" from production build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+const { config } = require("process");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 module.exports = {
+  reactStrictMode: true,
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        exclude: /\/app\/design-system\//,
+      });
+    }
+    return config;
+  },
+
   images: {
     remotePatterns: [
       {
