b867c6074057cdc87b76e5d96881bf215116e618
Adarsh Desai <desaiadarsh22@gmail.com>
2023-06-09 22:34:49 +0530

    refactor: Remove components from design system

--- app/design-system/page.tsx
+++ app/design-system/page.tsx
@@ -10,9 +10,6 @@ interface Option {
   value: string;
 }
 
-const REGIONS: Option[] = siteConfig.regions;
-const SORTINGPARAMS: Option[] = siteConfig.sortingParams;
-
 const page = () => {
   if (process.env.NODE_ENV !== "development") {
     // Render an alternative message or redirect to a different route in the production build
@@ -22,18 +19,6 @@ const page = () => {
   return (
     <div className="mx-[10vw] my-[5vh] flex flex-col gap-5">
       <ColorPalette />
-      <div className="flex gap-4">
-        <DropdownMenu
-          dropdownOptions={REGIONS}
-          dropdownLabel={"Filter by regions"}
-        />
-        <DropdownMenu
-          dropdownOptions={SORTINGPARAMS}
-          dropdownLabel={"Sort By"}
-        />
-      </div>
-      <CountryCardContainer />
-      <SearchBar />
     </div>
   );
 };
