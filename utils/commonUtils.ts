0a0a6f432e6b456d1e685f76dee6eb41a5ded0ac
Adarsh Desai <desaiadarsh22@gmail.com>
2023-06-17 15:21:35 +0530

    feat: Add getCountriesData function to data

--- utils/commonUtils.ts
+++ utils/commonUtils.ts
@@ -1,3 +1,14 @@
 export function formatPopulation(population: number): string {
   return population.toLocaleString();
 }
+
+export async function getCountriesData() {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
